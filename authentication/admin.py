ec0e771bec9c0bfc22ee63ca39b689893ed78955
otabek <baqara163@gmail.com>
2025-02-12 12:55:20 +0500

    User model updated

--- authentication/admin.py
+++ authentication/admin.py
@@ -5,4 +5,4 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ('telegram_id', 'is_active', 'role')
     list_display_links = ('telegram_id', 'role')
-    readonly_fields = ('telegram_id', 'role')
+    # readonly_fields = ('telegram_id', 'role')
