cdb220109754092fa2c2c4f45442de720f1b2387
otabek <baqara163@gmail.com>
2025-02-17 20:07:43 +0500

    User updated

--- sharing/models.py
+++ sharing/models.py
@@ -23,6 +23,7 @@ class Book(BaseModel):
     published_at = models.PositiveIntegerField()
     status = models.IntegerField(choices=BOOK_STATUS, default=1)
     isbn = models.CharField(max_length=20, blank=True, null=True)
+    view_count = models.BigIntegerField(default=0)
     like = models.ManyToManyField(to='authentication.User', related_name='book_likes', blank=True)
     is_default = models.BooleanField(default=False)
 
