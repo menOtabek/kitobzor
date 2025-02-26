b436dc8290a891a712005ecea2c180b76db060af
otabek <baqara163@gmail.com>
2025-02-27 02:26:13 +0500

    paginator updated

--- blog/paginators/posts_list_paginator.py
+++ blog/paginators/posts_list_paginator.py
@@ -2,7 +2,8 @@ from blog.serilalizers import PostListSerializer
 from django.core.paginator import Paginator
 
 def paginate_posts(post_query, context: dict, page_size: int, page_number: int):
-    total_count = post_query.count()
+    total_count = post_query.values('id').distinct().count()
+
     paginator = Paginator(post_query, page_size)
     posts = paginator.get_page(page_number)
 
