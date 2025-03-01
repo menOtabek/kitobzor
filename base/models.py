2cdc44faf8e7da0b9a1e2ffe43e2500c5f5489fc
otabek <baqara163@gmail.com>
2025-03-01 17:29:30 +0500

    FAQ and PrivacyPolicy added to base

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from abstract_model.base_model import BaseModel
+from tinymce.models import HTMLField
 
 
 class Region(BaseModel):
@@ -19,7 +20,25 @@ class District(BaseModel):
 
 class Banner(BaseModel):
     picture = models.ImageField(upload_to="banner/pictures/")
-    title = models.CharField(max_length=200, blank=True, null=True)
+    title = models.CharField(max_length=400, blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class FAQ(BaseModel):
+    question = models.CharField(max_length=400)
+    answer = HTMLField()
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.question
+
+
+class PrivacyPolicy(BaseModel):
+    title = models.CharField(max_length=400)
+    description = HTMLField()
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
