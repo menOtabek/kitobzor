2453d144b358f2959c33f1c8895db52168ac180c
otabek <baqara163@gmail.com>
2025-03-18 19:47:05 +0500

    post list updated

--- blog/admin.py
+++ blog/admin.py
@@ -3,13 +3,13 @@ from .models import Post, PostComment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('user', 'book_name', 'book_author')
+    list_display = ('user', 'book_name', 'book_author', 'is_active')
     list_filter = ('user', 'book_name')
     search_fields = ('user', 'book_name')
 
 
 @admin.register(PostComment)
 class PostCommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'post', 'comment')
+    list_display = ('user', 'post', 'comment', 'is_banned')
     list_filter = ('user', 'post')
     search_fields = ('user', 'post')
