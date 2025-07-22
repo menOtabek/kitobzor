0346dc9800c3f761b2c1bdd4cb2e79637e37d3ae
otabek <baqara163@gmail.com>
2025-07-22 12:03:15 +0500

    cors updated

--- config/settings.py
+++ config/settings.py
@@ -34,8 +34,9 @@ DEBUG = os.getenv('DJANGO_DEBUG', '1') == '1'
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 # cors allowed origins
-cors_origins = os.getenv('CORS_ALLOWED_ORIGINS', '')
-CORS_ALLOWED_ORIGINS = [origin.strip() for origin in cors_origins.split(',') if origin.strip()]
+# cors_origins = os.getenv('CORS_ALLOWED_ORIGINS', '')
+# CORS_ALLOWED_ORIGINS = [origin.strip() for origin in cors_origins.split(',') if origin.strip()]
+CORS_ALLOWED_ORIGINS=True
 
 
 
